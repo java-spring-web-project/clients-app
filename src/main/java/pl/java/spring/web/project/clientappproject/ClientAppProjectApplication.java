afb45e9a6e934321b04d3c081e1110f9db4c17f9
Paweł Oczadly <oczadlypawe@gmail.com>
2018-09-27 18:58:22 +0200

    Initial commit

---
+++ src/main/java/pl/java/spring/web/project/clientappproject/ClientAppProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.java.spring.web.project.clientappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAppProjectApplication.class, args);
+	}
+}
